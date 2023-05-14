72ca10ad6ec191d105ea78f10a7536d9b3b66cdf
subho99 <subhajitbasistha@gmail.com>
2023-05-14 14:45:19 +0530

    Portflio Website

---
+++ src/assets/index.js
@@ -0,0 +1,86 @@
+import logo from "./logo2.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import logosss from "./logoSSS.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import java from "./tech/java.png";
+import keras from "./tech/keras.png";
+import power from "./tech/powerbi.png";
+import pyt from "./tech/Python.png";
+import sql from "./tech/sql.png";
+import tensor from "./tech/Tensor.png";
+import sci from "./tech/scikit.png";
+import panda from "./tech/Pandas.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import ijn from "./company/IJN.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import dsp from "./DataScienceProjects.png";
+import sss from "./SSS.png";
+
+export {
+  logo,
+  logosss,
+  backend,
+  creator,
+  mobile,
+  sci,
+  panda,
+  java,
+  keras,
+  power,
+  pyt,
+  sql,
+  tensor,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  dsp,
+  sss,
+  ijn,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+import {
+  logo,
+  logosss,
+  backend,
+  creator,
+  mobile,
+  sci,
+  panda,
+  java,
+  keras,
+  power,
+  pyt,
+  sql,
+  tensor,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  dsp,
+  sss,
+  ijn,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "DataWarehouse Engineer",
+      icon: web,
+    },
+    {
+      title: "Data Scientist",
+      icon: mobile,
+    },
+    {
+      title: "Python Developer",
+      icon: backend,
+    },
+    {
+      title: "Frontend Developer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "Java",
+      icon: java,
+    },
+    {
+      name: "Python",
+      icon: pyt,
+    },
+    {
+      name: "PowerBI",
+      icon: power,
+    },
+    {
+      name: "Keras",
+      icon: keras,
+    },
+    {
+      name: "Tensor Flow",
+      icon: tensor,
+    },
+    {
+      name: "SQL",
+      icon: sql,
+    },
+    {
+      name: "Pandas",
+      icon: panda,
+    },
+    {
+      name: "Sci-Kit Learn ",
+      icon: sci,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Data Warehouse Engineer",
+      company_name: "Innovation Junction LLC.",
+      icon: ijn,
+      iconBg: "#383E56",
+      date: "Sep 2020 - Sep 2021",
+      points: [
+        "Scraping through Customer Data and processing the same for on-field inventory.",
+        "Automate processing through writing scripts and codes for Data Processing jobs.",
+        "Monitor Data Processing Fails and Resolve Issues.",
+        "Assist with Data Processing Issues with on-field Inventories.",
+      ],
+    },
+    {
+      title: "Customer Program Developer & DataWarehouse Expert",
+      company_name: "Innovation Junction LLC.",
+      icon: ijn,
+      iconBg: "#E6DEDD",
+      date: "Oct 2021 - Mar 2023",
+      points: [
+        "Develop Custom Inventory Management Solutions using Legacy Systems.",
+        "Collaborating with cross-functional teams to develop and put into production the inventory solutions.",
+        "Maintain constant channel of Knowledge Transfer with Client and New Trainees in active production process.",
+        "Manage the Data Processing team to be efficient and meet client expectations.",
+      ],
+    },
+    {
+      title: "Project Lead & Datawarehousing Expert",
+      company_name: "Innovation Junction LLC.",
+      icon: ijn,
+      iconBg: "#383E56",
+      date: "Apr 2023 - present",
+      points: [
+        "Lead DP and CPD team and client's first point of contact.",
+        "Collaborating with client and manage any urgent or emergency issues falling under the purview of DP and CPD.",
+        "Ensuring team meets SLA while maintaining quality of service.",
+        "Report to Customer Success and Relations Manager for any SLA violations and Emergencies.",
+        "Maintain effective workload distribution amongst team members",
+      ],
+    },
+    
+  ];
+  const education = [
+    {
+      title: "Data Warehouse Engineer",
+      company_name: "Innovation Junction LLC.",
+      icon: ijn,
+      iconBg: "#383E56",
+      date: "Sep 2020 - Sep 2021",
+      points: [
+        "Scraping through Customer Data and processing the same for on-field inventory.",
+        "Automate processing through writing scripts and codes for Data Processing jobs.",
+        "Monitor Data Processing Fails and Resolve Issues.",
+        "Assist with Data Processing Issues with on-field Inventories.",
+      ],
+    },
+    {
+      title: "Customer Program Developer & DataWarehouse Expert",
+      company_name: "Innovation Junction LLC.",
+      icon: ijn,
+      iconBg: "#E6DEDD",
+      date: "Oct 2021 - Mar 2023",
+      points: [
+        "Develop Custom Inventory Management Solutions using Legacy Systems.",
+        "Collaborating with cross-functional teams to develop and put into production the inventory solutions.",
+        "Maintain constant channel of Knowledge Transfer with Client and New Trainees in active production process.",
+        "Manage the Data Processing team to be efficient and meet client expectations.",
+      ],
+    },
+    {
+      title: "Project Lead & Datawarehousing Expert",
+      company_name: "Innovation Junction LLC.",
+      icon: ijn,
+      iconBg: "#383E56",
+      date: "Apr 2023 - present",
+      points: [
+        "Lead DP and CPD team and client's first point of contact.",
+        "Collaborating with client and manage any urgent or emergency issues falling under the purview of DP and CPD.",
+        "Ensuring team meets SLA while maintaining quality of service.",
+        "Report to Customer Success and Relations Manager for any SLA violations and Emergencies.",
+        "Maintain effective workload distribution amongst team members",
+      ],
+    },
+    
+  ];  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Seva Sangh Samiti",
+      description:
+        "Website for an NGO called Seva Sangh Samiti, which has been operating for over 55 years.",
+      tags: [
+        {
+          name: "Azure",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Wordpress",
+          color: "green-text-gradient",
+        },
+        {
+          name: "HTML & CSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: sss,
+      source_code_link: "https://www.sevasanghsamiti.org/",
+    },
+    {
+      name: "Data Science Projects",
+      description:
+        "Solve various Data Science Project highlighting different datasets and problems and different approaches to solutions.",
+      tags: [
+        {
+          name: "Python",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "MachineLearning",
+          color: "green-text-gradient",
+        },
+        {
+          name: "DataScience",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: dsp,
+      source_code_link: "https://github.com/subho99/Computational-Data-Science",
+    },
+    
+    
+  ];
+  
+  export { services, technologies, experiences, education, testimonials, projects };
